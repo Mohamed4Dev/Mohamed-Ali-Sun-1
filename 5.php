8407141193f30dfdef5854b69a65ba5f7e7cfcc0
Mohamed EL-7alag <44813634+Mohamed4Dev@users.noreply.github.com>
2021-02-28 03:31:57 +0200

    Own algorithm Q5

--- 5.php
+++ 5.php
@@ -2,11 +2,25 @@
 //----------------task Q
 // write a script that removes any duplicates from an array.
 
-$array = [1,2,3,5,6,5,5,7,6]; // original data
+$array = [1, 2, 3, 5, 6, 5, 5, 7, 6]; // original data
 print_r($array);
 
 echo "<br>";
 
-$filteredArray =array_unique($array); // duplicates removed data
+//Bulit in function 
+$filteredArray = array_unique($array); // duplicates removed data
 print_r($filteredArray);
+echo "<br> <hr>";
 
+//Own algorithm
+echo "Own algorithm <br>";
+$newArray=[];
+foreach ($array as $value) {
+    foreach ($newArray as $newVlue) {
+        if ($value == $newVlue) {
+            continue 2;               //
+        }
+    }
+    $newArray[] =$value;
+}
+print_r($newArray);
