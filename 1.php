2329ffcca90c61bb169f2079b0a824ad39a02fb0
Mohamed EL-7alag <44813634+Mohamed4Dev@users.noreply.github.com>
2021-02-28 03:12:32 +0200

    own algorithm Q1

--- 1.php
+++ 1.php
@@ -4,5 +4,27 @@
 // in the array.
 
 $array = [1, 2, 3, 4, 5, 6]; // array data
-echo "maximum is " . max($array) . " <br> <hr>"; // maximum number
+
+//built in functions
+echo "built in functions <br>";
+echo "maximum is " . max($array) . " <br>"; // maximum number
 echo "minimum is " . min($array);   // minimum number
+echo "<br> <hr>";
+
+//own algorithm
+echo "own algorithm <br>";
+$var = $array[0];
+for ($i = 0; $i < count($array); $i++) {
+    if ($var < $array[$i]) {
+        $var = $array[$i] . "<br>";               // maximum element
+    }
+}
+echo "Max is $var";
+
+
+for ($i = 0; $i < count($array); $i++) {
+    if ($var > $array[$i]) {
+        $var = $array[$i] . "<br>";               // minimum element
+    }
+}
+echo "Min is $var";
